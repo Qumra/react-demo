e631b6f066482491c8f6b7bc3f0b360084367f73
Qumra <39854812+Qumra@users.noreply.github.com>
2019-05-14 09:29:01 +0800

    Update Login.jsx

--- src/view/base/Login/Login.jsx
+++ src/view/base/Login/Login.jsx
@@ -1,61 +1,30 @@
-import React,{Component}from 'react';
-import {
-    Form, Icon, Input, Button, Checkbox,
-} from 'antd';
-
-import  './Login.css'
-import sdk from '../../../sdk1.js'
+import React, { Component } from "react";
+import cssObj from "./Login.css";
+import LoginNav from "@/components/LoginNav/LoginNav.jsx"
+import Logo from "@/components/Logo/Logo.jsx"
+import LoginContain from "@/components/LoginContain/LoginContain.jsx"
+import history from '@/config/history';
 class Login extends Component {
-    handleSubmit = (e) => {
-      e.preventDefault();
-      this.props.form.validateFields((err, values) => {
-        values.callback=function(res){
-          if(res.code!=0){
-            
-          }
+    constructor(){
+        super()
+        this.state = {
+        //   logoStyle:cssObj.logoImg
+         
         }
-        if (!err) {
-          console.log('Received values of form: ', values);
-          sdk.SMCSDK_User_Auth.login(values)
-        }
-      });
     }
-  
     render() {
-      const { getFieldDecorator } = this.props.form;
-      return (
-        <Form onSubmit={this.handleSubmit} className="login-form">
-          <Form.Item>
-            {getFieldDecorator('userName', {
-              rules: [{ required: true, message: 'Please input your username!' }],
-            })(
-              <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
-            )}
-          </Form.Item>
-          <Form.Item>
-            {getFieldDecorator('password', {
-              rules: [{ required: true, message: 'Please input your Password!' }],
-            })(
-              <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
-            )}
-          </Form.Item>
-          <Form.Item>
-            {getFieldDecorator('remember', {
-              valuePropName: 'checked',
-              initialValue: true,
-            })(
-              <Checkbox>Remember me</Checkbox>
-            )}
-            <a className="login-form-forgot" href="">Forgot password</a>
-            <Button type="primary" htmlType="submit" className="login-form-button">
-              Log in
-            </Button>
-            Or <a href="">register now!</a>
-          </Form.Item>
-        </Form>
-      );
+        return (
+            <div className={cssObj.Login}>
+                <LoginNav></LoginNav>
+                <div className={cssObj.bgDiv}>
+                <Logo  logoStyle="logoStyle"></Logo>
+                <LoginContain></LoginContain>
+                <button onClick= {()=> history.push({pathname:'/main'})}>跳转</button>
+                </div>
+            </div>
+        )
     }
-  }
-  
-   Login = Form.create({ name: 'Login' })(Login);
-  export default Login
+}
+
+
+export default Login
